e80081ccfd68c0707781cb15f7d571eaf9e3177e
Ava Manning <362579849@GoTVDSB.ca>
2025-10-23 08:53:35 -0400

    done

--- Program.cs
+++ Program.cs
@@ -10,76 +10,97 @@ namespace Topic_6
         {
             Random generator = new Random();
             int randNum; // We will store our random number in this variable 
-            //randNum = generator.Next();
-            //Console.WriteLine(randNum);
-            ////Console.WriteLine("My random number is " + randNum);
-            //Console.WriteLine("Here are some numbers from 0 to 4!");
-            //Console.Write(generator.Next(2, 5) + " ");
-            //Console.Write(generator.Next(2, 5) + " ");
-            //Console.Write(generator.Next(2, 5) + " ");
-            //Console.Write(generator.Next(2, 5) + " ");
-            //Console.Write(generator.Next(2, 5) + " ");
-            //Console.WriteLine(generator.Next(2, 5) + " ");
-            //Console.WriteLine("gkjhsdg");
+            randNum = generator.Next();
+            Console.WriteLine(randNum);
+            //Console.WriteLine("My random number is " + randNum);
+            Console.WriteLine("Here are some numbers from 0 to 4!");
+            Console.Write(generator.Next(2, 5) + " ");
+            Console.Write(generator.Next(2, 5) + " ");
+            Console.Write(generator.Next(2, 5) + " ");
+            Console.Write(generator.Next(2, 5) + " ");
+            Console.Write(generator.Next(2, 5) + " ");
+            Console.WriteLine(generator.Next(2, 5) + " ");
+            Console.WriteLine("gkjhsdg");
 
-            //Console.WriteLine("Here are some numbers from 0 to 99!");
-            //Console.Write(generator.Next(100) + " ");
-            //Console.Write(generator.Next(100) + " ");
-            //Console.Write(generator.Next(100) + " ");
-            //Console.Write(generator.Next(100) + " ");
-            //Console.Write(generator.Next(100) + " ");
-            //Console.WriteLine(generator.Next(100) + " ");
-
-
-            //int num1 = generator.Next(10);
-            //int num2 = generator.Next(10);
-
-            //if (num1 == num2)
-            //{
-            //    Console.WriteLine("The random numbers were the same! Weird.");
-            //}
-            //Console.ReadLine(); // Keeps the program from quitting 
-            //if (num1 != num2)
-            //{
-            //    Console.WriteLine("The random numbers were different! Not weird.");
-            //}
-            //Console.ReadLine(); // Keeps the program from quitting 
-
-            //int min, max;
-            //Console.WriteLine("Enter a min number");
-            //int.TryParse(Console.ReadLine(), out min);
-            //Console.WriteLine("Enter a max number");
-            //int.TryParse(Console.ReadLine(), out max);
-            //Console.WriteLine(generator.Next(min, max + 1));
+            Console.WriteLine("Here are some numbers from 0 to 99!");
+            Console.Write(generator.Next(100) + " ");
+            Console.Write(generator.Next(100) + " ");
+            Console.Write(generator.Next(100) + " ");
+            Console.Write(generator.Next(100) + " ");
+            Console.Write(generator.Next(100) + " ");
+            Console.WriteLine(generator.Next(100) + " ");
 
 
-            //int secretNumber, guess;
-            //secretNumber = generator.Next(1, 11);
+            int num1 = generator.Next(10);
+            int num2 = generator.Next(10);
 
-            //Console.WriteLine("Guess the secret number:");
-            //int.TryParse(Console.ReadLine(), out guess);
-            //if (guess == secretNumber)
-            //{
-            //    Console.WriteLine("You got it");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Wrong, it was " + secretNumber);
-            //}
+            if (num1 == num2)
+            {
+                Console.WriteLine("The random numbers were the same! Weird.");
+            }
+            Console.ReadLine(); // Keeps the program from quitting 
+            if (num1 != num2)
+            {
+                Console.WriteLine("The random numbers were different! Not weird.");
+            }
+            Console.ReadLine(); // Keeps the program from quitting 
+
+            int min, max;
+            Console.WriteLine("Enter a min number");
+            int.TryParse(Console.ReadLine(), out min);
+            Console.WriteLine("Enter a max number");
+            int.TryParse(Console.ReadLine(), out max);
+            Console.WriteLine(generator.Next(min, max + 1));
+
+
+            int secretNumber, guess;
+            secretNumber = generator.Next(1, 11);
+
+            Console.WriteLine("Guess the secret number:");
+            int.TryParse(Console.ReadLine(), out guess);
+            if (guess == secretNumber)
+            {
+                Console.WriteLine("You got it");
+            }
+            else
+            {
+                Console.WriteLine("Wrong, it was " + secretNumber);
+            }
 
 
             int diceOne, diceTwo;
             diceOne = generator.Next(1, 7);
             diceTwo = generator.Next(1, 7);
             Console.WriteLine("Roll both of the dice");
-            Console.WriteLine("What did you role on the first dice?");
-            int.TryParse(Console.ReadLine(), out diceOne);
-            Console.WriteLine("What did you role on the second dice?");
-            int.TryParse(Console.ReadLine(), out diceTwo);
+            Console.WriteLine("The first die: " + diceOne);
+            Console.WriteLine("The second die: " + diceTwo);
             Console.WriteLine("You rolled a total of " + (diceOne + diceTwo) + "!");
-            
 
+            int fortune;
 
+            Console.WriteLine("Welcome to the magic 8 ball, please enter a yes or no question.");
+            Console.ReadLine();
+            fortune = generator.Next(1, 6);
+            if (fortune == 1)
+            {
+                Console.WriteLine("Yes, Defintely!");
+            }
+            else if (fortune == 2)
+            {
+                Console.WriteLine("Definetly not!");
+            }
+            else if (fortune == 3)
+            {
+                Console.WriteLine("Ask again later");
+            }
+            else if (fortune == 4)
+            {
+                Console.WriteLine("Maybe");
+            }
+            else if (fortune == 5)
+            {
+                Console.WriteLine("Probably not!");
+            }
         }
     }
 }
